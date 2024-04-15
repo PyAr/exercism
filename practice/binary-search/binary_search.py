ec071cfad710bd407a1a4595da13b1bf4da451f3
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2024-04-15 10:58:58 +0530

    rest-api

--- practice/binary-search/binary_search.py
+++ practice/binary-search/binary_search.py
@@ -1,2 +1,14 @@
 def find(search_list, value):
-    pass
+    low = 0
+    high = len(search_list) - 1
+
+    while low <= high:
+        middle = (low + high) // 2
+        if search_list[middle] == value:
+            return middle
+        elif search_list[middle] > value:
+            high = middle - 1
+        else:
+            low = middle + 1
+
+    raise ValueError("Value not found in array")
